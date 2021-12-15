251198de23c5f2b1da6353fa1d514cfb97e2f915
Oscar Silva <oscar.reis2011@hotmail.com>
2021-12-15 19:44:43 -0300

    Create MissionSteps component to list the steps of the mission

---
+++ components/MissionSteps.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="steps-container">
+    <h2 id="step-title">Etapas</h2>
+    <h3 style="text-align: center" v-if="!steps">
+      Nenhuma etapa encontrada para essa missão :(
+    </h3>
+    <step-item v-for="step in steps" :step="step" :key="step.id" />
+  </div>
+</template>
+
+<script>
+import StepItem from "./StepItem.vue";
+
+export default {
+  components: { StepItem },
+  props: {
+    steps: Array,
+  },
+};
+</script>
+
+
+<style>
+.steps-container {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 800px;
+  min-height: 160px;
+  margin: 25px;
+  border-radius: 10px;
+  background-color: #fff;
+  padding: 5px;
+}
+.steps-container #step-title {
+  color: var(--secondaryColor);
+  text-align: center;
+}
+</style>
